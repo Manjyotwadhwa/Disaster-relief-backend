80e8080a3d14f7133961ee61cc1d8b7701c2cfe3
Manjyotwadhwa <2023.manjyotsingh.wadhwa@ves.ac.in>
2025-12-23 18:20:03 +0530

    feat: add MongoDB integration and report CRUD read APIs

--- index.js
+++ index.js
@@ -1,14 +1,98 @@
-const express = require("express");
-const app = express();
-app.use(express.json());
-app.post("/reports", (req, res) => {
-  const report = req.body;
+require("dotenv").config();
 
-  res.status(201).json({
-    message: "Disaster report received",
-    report: report
-  });
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+app.use(express.json())
+
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error(err));
+
+const reportSchema = new mongoose.Schema({
+  type: {
+    type: String,
+    required: true
+  },
+  location: {
+    type: String,
+    required: true
+  },
+  severity: {
+    type: String,
+    default: "unknown"
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
 });
+
+const Report = mongoose.model("Report", reportSchema);
+
+app.post("/reports", async (req, res) => {
+  try {
+    const { type, location, severity } = req.body;
+
+    if (!type || !location) {
+      return res.status(400).json({
+        error: "type and location are required"
+      });
+    }
+
+    const newReport = new Report({
+      type,
+      location,
+      severity
+    });
+
+    const savedReport = await newReport.save();
+
+    res.status(201).json({
+      message: "Disaster report saved",
+      report: savedReport
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      error: "Server error"
+    });
+  }
+});
+
+app.get("/reports", async (req, res) => {
+  try {
+    const reports = await Report.find();
+    res.json(reports);
+  } catch (error) {
+    res.status(500).json({
+      error: "Server error"
+    });
+  }
+});
+app.get("/reports/:id", async (req, res) => {
+  try {
+    const reportId = req.params.id;
+
+    const report = await Report.findById(reportId);
+
+    if (!report) {
+      return res.status(404).json({
+        error: "Report not found"
+      });
+    }
+
+    res.json(report);
+
+  } catch (error) {
+    res.status(400).json({
+      error: "Invalid report ID"
+    });
+  }
+});
+
 
 app.get("/", (req, res) => {
   res.send("Backend is running");
